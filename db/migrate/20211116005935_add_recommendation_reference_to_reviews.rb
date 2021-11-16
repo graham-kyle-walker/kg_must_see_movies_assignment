e09c6f55964fcba75d3d4ddcf6e8751d08d4b104
anonymous anonymous <kyle.graham@kellogg.northwestern.edu>
2021-11-16 00:59:35 +0000

    Add foreign key movie_id reference to reviews table

---
+++ db/migrate/20211116005935_add_recommendation_reference_to_reviews.rb
@@ -0,0 +1,7 @@
+class AddRecommendationReferenceToReviews < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :reviews, :movies
+    add_index :reviews, :movie_id
+    change_column_null :reviews, :movie_id, false
+  end
+end
