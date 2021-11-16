151ba05821f945c9be207c4e5692c76ff026e6c2
anonymous anonymous <kyle.graham@kellogg.northwestern.edu>
2021-11-16 00:59:32 +0000

    Add foreign key movie_id reference to bookmarks table

---
+++ db/migrate/20211116005932_add_collection_reference_to_bookmarks.rb
@@ -0,0 +1,7 @@
+class AddCollectionReferenceToBookmarks < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :bookmarks, :movies
+    add_index :bookmarks, :movie_id
+    change_column_null :bookmarks, :movie_id, false
+  end
+end
